3944506921f1174e75580c58655f46b47bacaf9f
EmalisaAntonioli <r0910184@student.thomasmore.be>
2024-02-27 15:42:12 +0100

    27/2/24

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TripManager.Data;
+using TripManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TripManager.Controllers
+{
+    public class LocationController : Controller
+    {
+        private readonly TripDbContext _context;
+        public LocationController(TripDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Location> locations = _context.Locations.ToList();
+            return View(locations);
+        }
+    }
+}
